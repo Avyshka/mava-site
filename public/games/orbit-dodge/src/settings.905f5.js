cb75b5114bc037743321f0d0d09e564e7e8ba351
Aivar Sergeev <avyshka@yandex.ru>
2026-05-11 22:42:42 +0200

    Updated orbit dodge game

---
+++ public/games/orbit-dodge/src/settings.905f5.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Boot.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",resources:"dd764",main:"5e531"}};
